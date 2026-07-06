148330e4aa06e588f175578226cfd566051c1ca5
nix <nix@nix.com>
2026-07-05 21:48:11 -0600

    feat: tmux gruvbox

--- modules/features/tmux-conf.nix
+++ modules/features/tmux-conf.nix
@@ -6,6 +6,15 @@
         shell = "${pkgs.fish}/bin/fish";
         terminal = "tmux-256color";
         historyLimit = 100000;
+
+        plugins = with pkgs; [
+          {
+            plugin = tmuxPlugins.gruvbox;
+            extraConfig = "set -g @tmux-gruvbox 'dark256'";
+          }
+        ];
+
+        extraConfig = "";
       };
     };
   };
