963b3ec1acd9b5209e443c83fe4fae6db4fd3020
nix <nix@nix.com>
2026-07-05 22:29:31 -0600

    fix: tmux keymaps

--- modules/features/tmux-conf.nix
+++ modules/features/tmux-conf.nix
@@ -14,7 +14,18 @@
           }
         ];
 
-        extraConfig = "";
+        extraConfig = ''
+          set -g status-position top
+          set -g mode-keys vi
+          bind -n M-Left  select-pane -L
+          bind -n M-Right select-pane -R
+          bind -n M-Up    select-pane -U
+          bind -n M-Down  select-pane -D
+          bind -n M-n     next-window
+          bind -n M-p     previous-window
+          bind '\'         split-window -h
+          bind -         split-window -v
+        '';
       };
     };
   };
