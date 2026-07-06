ff447febd5710597277c6a3c4e8b043acdd1598b
nix <nix@nix.com>
2026-07-05 22:46:47 -0600

    fix: starship format

--- modules/features/starship-conf.nix
+++ modules/features/starship-conf.nix
@@ -7,36 +7,7 @@
         settings = {
           "$schema" = "https://starship.rs/config-schema.json";
 
-          format = ''
-            [](color_orange)\
-            $os\
-            $username\
-            [](bg:color_yellow fg:color_orange)\
-            $directory\
-            [](fg:color_yellow bg:color_aqua)\
-            $git_branch\
-            $git_status\
-            [](fg:color_aqua bg:color_blue)\
-            $c\
-            $cpp\
-            $rust\
-            $golang\
-            $nodejs\
-            $bun\
-            $php\
-            $java\
-            $kotlin\
-            $haskell\
-            $python\
-            [](fg:color_blue bg:color_bg3)\
-            $docker_context\
-            $conda\
-            $pixi\
-            [](fg:color_bg3 bg:color_bg1)\
-            $time\
-            [ ](fg:color_bg1)\
-            $line_break$character
-          '';
+          format = "[](color_orange)$os$username[](bg:color_yellow fg:color_orange)$directory[](fg:color_yellow bg:color_aqua)$git_branch$git_status[](fg:color_aqua bg:color_blue)$c$cpp$rust$golang$nodejs$bun$php$java$kotlin$haskell$python[](fg:color_blue bg:color_bg3)$docker_context$conda$pixi[](fg:color_bg3 bg:color_bg1)$time[ ](fg:color_bg1)$line_break$character";
 
           palette = "gruvbox_dark";
 
