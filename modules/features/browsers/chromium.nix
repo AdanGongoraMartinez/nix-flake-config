6fcc81957f056b92d34e90d45ccd85693a22f52e
adan <adangonmar@gmail.com>
2026-07-15 16:47:07 -0600

    fix: chromium options

--- modules/features/browsers/chromium.nix
+++ modules/features/browsers/chromium.nix
@@ -4,12 +4,20 @@
   ...
 }: {
   flake.nixosModules.chromium = {pkgs, ...}: {
-    home-manager.users.nix = {pkgs, ...}: {
-      programs.chromium = {
-        enable = true;
-        extensions = [
-          "cjpalhdlnbpafiamejdnhcphjbkeiagm;https://clients2.google.com/service/update2/crx" # ublock origin
-          "eimadpbcbfnmbkopoojfekhnkhdbieeh;https://clients2.google.com/service/update2/crx" # dark reader
+    programs.chromium = {
+      enable = true;
+      extensions = [
+        "cjpalhdlnbpafiamejdnhcphjbkeiagm;https://clients2.google.com/service/update2/crx" # ublock origin
+        "eimadpbcbfnmbkopoojfekhnkhdbieeh;https://clients2.google.com/service/update2/crx" # dark reader
+      ];
+      extraOpts = {
+        "WebAppInstallForceList" = [
+          {
+            "custom_name" = "Youtube";
+            "create_desktop_shortcut" = false;
+            "default_launch_container" = "window";
+            "url" = "https://youtube.com";
+          }
         ];
       };
     };
