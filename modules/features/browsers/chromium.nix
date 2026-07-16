01a099b880574e3b6d279305613407c532cbfbde
adan <adangonmar@gmail.com>
2026-07-15 18:02:41 -0600

    fix: chromium installation and remove webapp

--- modules/features/browsers/chromium.nix
+++ modules/features/browsers/chromium.nix
@@ -10,16 +10,22 @@
         "cjpalhdlnbpafiamejdnhcphjbkeiagm;https://clients2.google.com/service/update2/crx" # ublock origin
         "eimadpbcbfnmbkopoojfekhnkhdbieeh;https://clients2.google.com/service/update2/crx" # dark reader
       ];
-      extraOpts = {
-        "WebAppInstallForceList" = [
-          {
-            "custom_name" = "Youtube";
-            "create_desktop_shortcut" = false;
-            "default_launch_container" = "window";
-            "url" = "https://youtube.com";
-          }
+      # extraOpts = {
+      #   "WebAppInstallForceList" = [
+      #     {
+      #       "custom_name" = "Youtube";
+      #       "create_desktop_shortcut" = false;
+      #       "default_launch_container" = "window";
+      #       "url" = "https://youtube.com";
+      #     }
+      #   ];
+      # };
+
+      home-manager.users.nix = {pkgs, ...}: {
+        home.packages = with pkgs; [
+          chromium
         ];
-      };
-    };
+      };     
+     };
   };
 }
