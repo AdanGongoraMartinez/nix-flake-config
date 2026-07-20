e0fb285f60d9f2a75db9ff432d828c782ee6da34
adan <adangonmar@gmail.com>
2026-07-20 15:42:20 -0600

    fix: zed config

--- modules/features/work-utils/zed.nix
+++ modules/features/work-utils/zed.nix
@@ -3,7 +3,21 @@
     home-manager.users.nix = {pkgs, ...}: {
       programs.zed= {
         enable = true;
+        extensions = [ "nix" "toml" "rust" ];
+        userSettings = {
+          theme = {
+            mode = "system";
+            dark = "Gruvbox Dark";
+            light = "Gruvbox Light";
+          };
+          hour_format = "hour24";
+          vim_mode = true;
+        };
       };
+
+      home.packages = with pkgs; [
+        zed-editor
+      ];
     };
   };
 }
