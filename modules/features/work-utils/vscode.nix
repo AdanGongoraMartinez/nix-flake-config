b9712001622abd51a15edec91fa67ad774d2bfe5
adan <adangonmar@gmail.com>
2026-07-20 12:23:27 -0600

    fix: vscode extensions

--- modules/features/work-utils/vscode.nix
+++ modules/features/work-utils/vscode.nix
@@ -1,16 +1,14 @@
-{
-  self,
-  inputs,
-  ...
-}: {
-  flake.nixosModules.vscode = {pkgs, ...}: {
+{...}: {
+  flake.nixosModules.vscode = {...}: {
     home-manager.users.nix = {pkgs, ...}: {
       programs.vscode = {
         enable = true;
-        extensions = with pkgs.vscode-extensions; [
-          dracula-theme.theme-dracula
+        profiles.default.extensions = with pkgs.vscode-extensions; [
+          jdinhlife.gruvbox
           vscodevim.vim
           yzhang.markdown-all-in-one
+          bbenoist.nix
+          aaron-bond.better-comments
         ];
       };
     };
