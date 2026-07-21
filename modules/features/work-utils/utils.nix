87186d722b84b24980639d24a4df526b1a7f12c6
adan <adangonmar@gmail.com>
2026-07-21 14:11:49 -0600

    feat: pomodoro

--- modules/features/work-utils/utils.nix
+++ modules/features/work-utils/utils.nix
@@ -1,13 +1,10 @@
-{
-  self,
-  inputs,
-  ...
-}: {
-  flake.nixosModules.utils = {pkgs, ...}: {
+{...}: {
+  flake.nixosModules.utils = {...}: {
     home-manager.users.nix = {pkgs, ...}: {
       home.packages = with pkgs; [
         gnome-disk-utility
         gnome-connections
+        gnome-pomodoro
         gcolor3
       ];
     };
