d4ab21bb200a05a99a493b23fbd26562d8958471
adan <adangonmar@gmail.com>
2026-07-28 09:03:07 -0600

    feat: auto flatpak

--- modules/host/work-and-game-station/configuration.nix
+++ modules/host/work-and-game-station/configuration.nix
@@ -70,6 +70,13 @@
 
     # Enable Flatpak and declarative packages
     services.flatpak.enable = true;
+    systemd.services.flatpak-repo = {
+    wantedBy = [ "multi-user.target" ];
+    path = [ pkgs.flatpak ];
+    script = ''
+      flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+    '';
+  };
 
     # Enable CUPS to print documents.
     # services.printing.enable = true;
