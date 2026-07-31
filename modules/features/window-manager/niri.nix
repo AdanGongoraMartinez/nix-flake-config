c160be54111a753a84a23138a9801dab1afb47c9
adan <adangonmar@gmail.com>
2026-07-31 17:37:52 -0600

    fix: niri libdisplay commented

--- modules/features/window-manager/niri.nix
+++ modules/features/window-manager/niri.nix
@@ -9,19 +9,18 @@
   perSystem = { pkgs, lib, self', ... }: {
     packages.myNiri = inputs.wrapper-modules.wrappers.niri.wrap {
       inherit pkgs;
-      # libdisplay 0.3 needed
-      package = pkgs.niri.override {
-        libdisplay-info = pkgs.libdisplay-info.overrideAttrs (finalAttrs: {
-          version = "0.3.0";
-          src = pkgs.fetchFromGitLab {
-            domain = "gitlab.freedesktop.org";
-            owner = "emersion";
-            repo = "libdisplay-info";
-            rev = finalAttrs.version;
-            sha256 = "sha256-nXf2KGovNKvcchlHlzKBkAOeySMJXgxMpbi5z9gLrdc=";
-          };
-        });
-      };
+      # package = pkgs.niri.override {
+      #   libdisplay-info = pkgs.libdisplay-info.overrideAttrs (finalAttrs: {
+      #     version = "0.3.0";
+      #     src = pkgs.fetchFromGitLab {
+      #       domain = "gitlab.freedesktop.org";
+      #       owner = "emersion";
+      #       repo = "libdisplay-info";
+      #       rev = finalAttrs.version;
+      #       sha256 = "sha256-nXf2KGovNKvcchlHlzKBkAOeySMJXgxMpbi5z9gLrdc=";
+      #     };
+      #   });
+      # };
 
       settings = {
         spawn-at-startup = [
